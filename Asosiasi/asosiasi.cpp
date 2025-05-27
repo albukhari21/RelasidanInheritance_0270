97d9e1f3e42a93739f17dd8e547e8dc931e6f755
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-27 11:57:43 +0700

    Menambahkan class dokter dengan asosiasi ke pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -21,3 +21,19 @@ public:
     void cetakDokter();
 };
 
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien* pPasien);
+    void cetakPasien();
+};
